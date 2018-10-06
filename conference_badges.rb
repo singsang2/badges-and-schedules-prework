40432f6ab72e7e4f2c345c02d34e28c0369d8b12
Sung Bae <singsang2@gmail.com>
2018-10-06 20:37:52 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,4 +12,11 @@ end
 
 def assign_rooms(list_speaker)
   list_speaker.each_with_index do |speaker, index|
-    puts "Hello, "
+    puts "Hello, #{speaker}! You'll be assigned to room #{index+1}!"
+  end
+end
+
+def printer(list)
+  batch_badge_creator(list)
+  assign_rooms(list)
+end
