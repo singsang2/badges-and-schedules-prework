968a55970d8970e7f5ebf8e744376e30cfb275bc
Sung Bae <singsang2@gmail.com>
2018-10-06 20:36:12 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,15 @@
 # Write your code here.
-def badge_maker
-  
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
 end
+
+
+def batch_badge_creator(list_name)
+  list_name.each do |name|
+    badge_maker(name)
+  end
+end
+
+def assign_rooms(list_speaker)
+  list_speaker.each_with_index do |speaker, index|
+    puts "Hello, "
