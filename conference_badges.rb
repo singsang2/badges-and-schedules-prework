90e0cb847ef3240592e454965494289e7d7004c0
Sung Bae <singsang2@gmail.com>
2018-10-06 20:40:07 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,9 +9,11 @@ def batch_badge_creator(list_name)
   list_name.each do |name|
     list.push(badge_maker(name))
   end
+  list
 end
 
 def assign_rooms(list_speaker)
+  list = []
   list_speaker.each_with_index do |speaker, index|
     list.push("Hello, #{speaker}! You'll be assigned to room #{index+1}!")
   end
